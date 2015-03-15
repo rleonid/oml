fc1d9cab699374e4a2dc2e22bda284bf0f6e7a77
Leonid Rozenberg <leonidr@gmail.com>
2015-03-14 23:58:11 -0400

    Add interface to descriptive.

---
+++ src/oml/descriptive.mli
@@ -0,0 +1,27 @@
+val mean : float array -> float
+val median : float array -> float
+val var : float array -> float
+val unbiased_var : float array -> float
+val cov : float array -> float array -> float
+val correlation : float array -> float array -> float
+val auto_correl : int -> float array -> float
+val moment : int -> float array -> float
+val skew : float array -> float
+val kurtosis : float array -> float
+val unbiased_skew : float array -> float
+val unbiased_kurtosis : float array -> float
+val var_standard_error : 'a array -> float
+val skew_standard_error : 'a array -> float
+val kurtosis_standard_error : 'a array -> float
+val var_statistic : float array -> float
+val skew_statistic : float array -> float
+val kurtosis_statistic : float array -> float
+val classify_skew : float array -> string
+val classify_kurtosis : float array -> string
+val stat_classify : float array -> (string * float) list
+val unbiased_classify : float array -> (string * float) list
+val unbiased_distribution_commentary : float array -> (string * float * string) list
+type dist_stats 
+val dist_classify : float array -> dist_stats
+val unbiased_dist_classify : float array -> dist_stats
+val histogram : float array -> float -> (float * int) array
