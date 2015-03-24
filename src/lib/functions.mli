a869f0a0162b9a1464a7d83301b931bc99f656a1
Leonid Rozenberg <leonidr@gmail.com>
2015-03-24 18:21:00 -0400

    Interface to Functions.

---
+++ src/lib/functions.mli
@@ -0,0 +1,12 @@
+(** Implementations of basic functions needed to compute distributions. *)
+
+val ln_gamma : float -> float
+val beta_func : float -> float -> float
+val gammap : float -> float -> float
+val gammaq : float -> float -> float
+val erf_taylor : float -> int -> float
+val erf : float -> float
+val erfc : float -> float
+val chi_square_less : float -> int -> float
+val chi_square_greater : float -> int -> float
+val t_lookup : float -> int -> float
