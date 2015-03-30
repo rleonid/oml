36a56e9550b0a0c80459d1286e301b4f0c8778d3
carm <carmelo.piccione@gmail.com>
2015-03-30 01:34:42 -0400

    uniform sampling function

--- src/lib/sampling.mli
+++ src/lib/sampling.mli
@@ -8,6 +8,13 @@ val normal : ?seed:int array -> mean:float -> std:float -> unit -> (unit ->
 (** [normal_std seed ()] is equivalent to [normal seed ~mean:0.0 ~std:1.0 ()].*)
 val normal_std : ?seed:int array -> unit -> (unit -> float)
 
+(** [uniform ?seed n] creates a generator that will return an integer
+    representating the ith element of [n] possible elements from
+    the uniformly random distribution.
+
+    @raise Invalid_argument if [n] is zero or negative. *)
+val uniform : ?seed:int array -> int -> (unit -> int)
+
 (** [multinomial ?seed weights] creates a generator that will return an integer
     representating the ith element from the Multinomial distribution given by
     a [weights] vector which sums to [1].
