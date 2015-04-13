772efae4cb2f41f30f07a47577aa930e7a66e7f6
Leonid Rozenberg <leonidr@gmail.com>
2015-04-13 12:29:22 -0400

    Add basic vector operations.

--- src/lib/oml.ml
+++ src/lib/oml.ml
@@ -1,4 +1,5 @@
 module Util = Util
+module Matrices = Matrices
 module Functions = Functions
 module Continued_fraction = Continued_fraction
 module Distributions = Distributions
--- src/lib/test_utils.ml
+++ src/lib/test_utils.ml
@@ -5,10 +5,15 @@ module Gen = struct
   module KG = Kaputt.Generator
   let pos_float = KG.(filter ((<=) 0.0) float)
   let neg_float = KG.(filter ((>=) 0.0) float)
+  (* Fixed length *)
   let array_fl n  = 
     let msg = "array length " ^ (string_of_int n) in
     KG.(array (lift n msg))
   let matrix r c e = KG.(array r (array_fl c e))
+
+  let array_float n = array_fl n KG.float
+  let matrix_float r c = matrix r c KG.float
+
   include Kaputt.Abbreviations.Gen
 end
 
