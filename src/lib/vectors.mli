08f161e0dfbf54dbeb99fa10e19d0d4d36f12002
Leonid Rozenberg <leonidr@gmail.com>
2015-04-14 13:45:00 -0400

    Use module type style.

--- src/lib/vectors.mli
+++ src/lib/vectors.mli
@@ -1,19 +1,19 @@
 (** Provide an interface to treat float array's as vectors. *)
 
-type vector = float array
+type t = float array
 
 (** [equal d x y] two vectors are equal if they have the same length and all
   pairwise elements are not [Util.significantly_different_from ?d] . *)
-val equal : ?d:float -> vector -> vector -> bool
+val equal : ?d:float -> t -> t -> bool
 
 (** [add x y] add pairwise elements. *)
-val add : vector -> vector -> vector
+val add : t -> t -> t
 
 (** [sub x y] subtract pairwise elements. *)
-val sub : vector -> vector -> vector
+val sub : t -> t -> t
 
 (** [mult s v] multiplies each element of [v] by scalar [s]. *)
-val mult : float -> vector -> vector
+val mult : float -> t -> t
 
 (** [dot x y] computes dot product. *)
-val dot : vector -> vector -> float
+val dot : t -> t -> float
