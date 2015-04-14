76d3cc3c8b616f07bea4bab8e9f7ce74cf801911
Leonid Rozenberg <leonidr@gmail.com>
2015-04-14 14:22:54 -0400

    Matrix products.

--- src/lib/matrices.ml
+++ src/lib/matrices.ml
@@ -1,7 +1,7 @@
 
 open Util
 
-type matrix = Vectors.vector array
+type t = Vectors.t array
 
 let equal ?d x y =
   let n = Array.length x
@@ -15,3 +15,58 @@ let add x y = Array.init (Array.length x) (fun i -> Vectors.add x.(i) y.(i))
 let sub x y = Array.init (Array.length x) (fun i -> Vectors.sub x.(i) y.(i))
 
 let mult x = Array.map (Vectors.mult x)
+
+let dim m =
+  Array.length m, Array.length m.(0)
+
+let identity n =
+  Array.init n (fun i ->
+    Array.init n (fun j ->
+      if i = j then 1.0 else 0.0))
+
+let prod ml mr =
+  let row_l, col_l = dim ml
+  and row_r, col_r = dim mr in
+  if col_l <> row_r then
+    invalidArg "incompatible matrices dimensions (%d, %d)*(%d, %d) for product"
+      row_l col_l row_r col_r
+  else
+    let n = col_l - 1 in
+    let s = ref 0.0 in
+    Array.init row_l (fun r ->
+      Array.init col_r (fun c ->
+        s := 0.0;
+        for i = 0 to n do
+          s := !s +. ml.(r).(i) *. mr.(i).(c)
+        done;
+        !s))
+
+let prod_row_vector v m =
+  let col_l = Array.length v and row_r, col_r = dim m in
+  if col_l <> col_r then
+    invalidArg "incompatible vector matrix dimensions [%d]*(%d, %d) for prod_row_vector"
+      col_l row_r col_r
+  else
+    let n = col_l - 1 in
+    let s = ref 0.0 in
+    Array.init col_l (fun c ->
+      s := 0.0;
+      for i = 0 to n do
+        s := !s +. v.(i) *. m.(i).(c)
+      done;
+      !s)
+
+let prod_column_vector m v =
+  let row_l, col_l = dim m and row_r = Array.length v in
+  if col_l <> row_r then
+    invalidArg "incompatible vector matrix dimensions (%d, %d)*[%d] for prod_row_vector"
+      row_l col_l row_r
+  else
+    let n = row_l - 1 in
+    let s = ref 0.0 in
+    Array.init col_l (fun c ->
+      s := 0.0;
+      for i = 0 to n do
+        s := !s +. v.(i) *. m.(c).(i)
+      done;
+      !s)
--- src/lib/matrices.mli
+++ src/lib/matrices.mli
@@ -1,17 +1,32 @@
 (** Provide a thin matrix interfaces for [float array array]'s. *)
 
-type matrix = Vectors.vector array
+type t = Vectors.t array
 
 (** [equal d x y] two matrices are equal if they have the same dimensions
   and all pairwise elements are not [Util.significantly_different_from ?d]
   from each other. *)
-val equal : ?d:float -> matrix -> matrix -> bool
+val equal : ?d:float -> t -> t -> bool
 
 (** [add x y] add two matrices. *)
-val add : matrix -> matrix -> matrix
+val add : t -> t -> t
 
 (** [sub x y] subtraction. *)
-val sub : matrix -> matrix -> matrix
+val sub : t -> t -> t
 
 (** [mult s v] scalar multiplication. *)
-val mult : float -> matrix -> matrix
+val mult : float -> t -> t
+
+(** [dim m] the dimensions (row, columns) of the matrix [m]. *)
+val dim : t -> int * int
+
+(** [identity n] create the identity matrix of rank [n]. *)
+val identity : int -> t
+
+(** [prod m n] matrix product [m * n]
+
+    @raise Invalid_argument if matrix sizes are not compatible. *)
+val prod : t -> t -> t
+
+val prod_row_vector : Vectors.t -> t -> Vectors.t
+
+val prod_column_vector : t -> Vectors.t -> Vectors.t
