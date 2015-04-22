2baea30250e9e62a38f30e46e24b5a6c09110cfd
Leonid Rozenberg <leonidr@gmail.com>
2015-04-22 11:30:22 -0400

    Improve documentation.

--- src/lib/running.mli
+++ src/lib/running.mli
@@ -15,9 +15,9 @@ val empty : t
 (** [init x] initializes a [t] with [x]. *)
 val init : float -> t
 
-(** [update t x] incorporate [x] into the statistics tracked in [t] *)
+(** [update t x] incorporate [x] into the statistics tracked in [t]. *)
 val update : t -> float -> t
 
-(** [join t1 t2] return a running stat if you had first observed the elements
+(** [join t1 t2] return a [Running.t] if you had first observed the elements
     by [t1] and then [t2]. *)
 val join : t -> t -> t
