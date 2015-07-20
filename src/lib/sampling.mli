7907ad7934f774b504f554793af0b14e9ffd7d51
Leonid Rozenberg <leonidr@gmail.com>
2015-07-20 15:02:21 -0400

    Adding a generator type to make signatures easier to understand.

--- src/lib/sampling.mli
+++ src/lib/sampling.mli
@@ -1,47 +1,51 @@
 (** Create generators for sampling from specified distributions. *)
 
+type 'a generator = unit -> 'a
+
 (** [uniform_i ?seed n] creates a generator that will return an integer
     with equal probabilities from the interval [0,n).
 
     @raise Invalid_argument if [n] is zero or negative, or above [2^30 - 1]. *)
-val uniform_i : ?seed:int array -> int -> (unit -> int)
+val uniform_i : ?seed:int array -> int -> int generator
 
 (** [uniform_f ?seed n] creates a generator that will return a float
     with equal probabilities from the interval [0,n].
 
     @raise Invalid_argument if [n] is zero or negative. *)
-val uniform_f : ?seed:int array -> float -> (unit -> float)
+val uniform_f : ?seed:int array -> float -> float generator
 
 (** [normal ?seed mean std ()] creates a generator that will return variables
-    from the Normal distribution of [mean] and [std] (standard deviation).*)
-val normal : ?seed:int array -> mean:float -> std:float -> unit -> (unit ->
-  float)
+    from the Normal distribution of [mean] and [std] (standard deviation).
+
+    @raise Invalid_argument if [std] is less than zero. *)
+val normal : ?seed:int array -> mean:float -> std:float -> unit -> float generator
 
 (** [normal_std seed ()] is equivalent to [normal seed ~mean:0.0 ~std:1.0 ()].*)
-val normal_std : ?seed:int array -> unit -> (unit -> float)
+val normal_std : ?seed:int array -> unit -> float generator
 
 (** [multinomial ?seed weights] creates a generator that will return an integer
     representating the ith element from the Multinomial distribution given by
     a [weights] vector which sums to [1].
 
-    @raise Invalid_argument if [weights] do not sum to [1.0] *)
-val multinomial : ?seed:int array -> float array -> (unit -> int)
+    @raise Invalid_argument if [weights] do not sum to [1.0] or any individual
+     weight is not in [0,1). *)
+val multinomial : ?seed:int array -> float array -> int generator
 
 (** [softmax ?seed ?temp weights] creates a generator that will return an integer
     representating the ith element from the softmax distribution given by
     a [weights] vector and [temp]erature parameter which defaults to [1.0].
 
-    @raise Invalid_argument if [weights] is empty *)
-val softmax : ?seed:int array -> ?temp:float -> float array -> (unit -> int)
+    @raise Invalid_argument if [weights] is empty. *)
+val softmax : ?seed:int array -> ?temp:float -> float array -> int generator
 
-(** Provides polymorphic versions that sample over arrays of any type *)
+(** Provides polymorphic versions that sample over arrays of any type. *)
 module Poly :
   sig
     (** [uniform ?seed elems] creates a generator that will sample from the
         [elems] array using the uniformly random distribution.
 
         @raise Invalid_argument if the given element array is empty. *)
-    val uniform : ?seed:int array -> 'a array -> (unit -> 'a)
+    val uniform : ?seed:int array -> 'a array -> 'a generator
 
     (** [multinomial ?seed elems weights] creates a generator will sample from
         the [elems] array using Multinomial distribution given by
@@ -49,7 +53,7 @@ module Poly :
 
         @raise Invalid_argument if [weights] do not sum to [1.0] or
         the length of the [elems] and [weights] arrays are not equal. *)
-    val multinomial : ?seed:int array -> 'a array -> float array -> (unit -> 'a)
+    val multinomial : ?seed:int array -> 'a array -> float array -> 'a generator
 
     (** [softmax ?seed ?temp elems weights] creates a generator that will
         sample from the [elems] array using the softmax distribution given by
@@ -58,5 +62,5 @@ module Poly :
         @raise Invalid_argument if [weights] is empty or the length of the
         [elems] and [weights] arrays are not equal. *)
     val softmax : ?seed:int array -> ?temp:float ->'a array ->
-      float array -> (unit -> 'a)
+      float array -> 'a generator
   end
