133a80aa3ace575db7f7be94225abc926aaace5e
Leonid Rozenberg <leonidr@gmail.com>
2016-02-03 10:22:44 -0500

    Organize for presentation

--- src/scripts/mnist_test.ml
+++ src/scripts/mnist_test.ml
@@ -7,10 +7,44 @@
    train 0.934 correct
    test 0.926 correct
 *)
+
+(** To visualize original values
+
+let showme ?(data=m_train) i =
+  let open Bigarrayo in
+  let x_i, y_i = Mnist.decode data i in
+  let x_256 =
+    let g = genarray_of_array2 x_i in
+    GA.init Int8_unsigned Fortran_layout [|28; 28|]
+        (fun i -> truncate (255. *. GA.get g i))
+  in
+  let label =
+    let rec loop i = if y_i.{i} = 1. then i else loop (i + 1)
+    in loop 1
+  in
+  x_256, label
+
+*)
+ 
+(****** DID YOU REQUIRE #DSFO ?!? 
+#require "dsfo" 
+*****)
 open Lacaml.D
 
-let feature_size = 28 * 28
+open Oml.Classification
+module P = Probabilities
 
+let m_train = Mnist.data ~dir:"../dsfo" `Train
+let m_test = Mnist.data ~dir:"../dsfo" `Test
+
+
+(******* End 1st Part  *******)
+
+(*
+#show_module_type  Classification.Intf.Continuous_encoded_data ;;
+*)
+
+let feature_size = 28 * 28
 module MnistEncoded =
   struct
     type clas = int
@@ -19,12 +53,7 @@ module MnistEncoded =
     let size = feature_size
   end
 
-open Oml.Classification
-module P = Probabilities
-module MnistLr = Logistic_regression.Multiclass(MnistEncoded)
-
-let m_train = Mnist.data ~dir:"../dsfo" `Train
-let m_test = Mnist.data ~dir:"../dsfo" `Test
+(******* End 2nd Part  *******)
 
 let column_to_label c =
   let rec loop i =
@@ -41,6 +70,7 @@ let to_samples data =
   |> Array.to_list
 
 let s_train = to_samples m_train
+let s_test = to_samples m_test
 
 let tolerance =
   if not (!Sys.interactive) && Array.length Sys.argv = 2 then
@@ -48,7 +78,13 @@ let tolerance =
   else
     1e7
 
+(****** End 3rd Part *****)
+
+module MnistLr = Logistic_regression.Multiclass(MnistEncoded)
+
 let mnist_lr = MnistLr.(estimate ~opt:(opt ~tolerance ()) s_train)
+
+(****** End 4th Part *****)
 
 let perf =
   s_train
@@ -57,8 +93,6 @@ let perf =
 let correct = List.fold_left (fun c (a,p) -> if a = p then c + 1 else c) 0 perf
 let total = List.length perf
 let () = Printf.printf "train %0.3f correct\n" ((float correct) /. (float total))
-
-let s_test = to_samples m_test
 
 let perf_test =
   s_test
