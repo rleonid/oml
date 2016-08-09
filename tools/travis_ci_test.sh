1e6f48e238bb5220de988649b4738e38df206bb6
Leonid Rozenberg <leonidr@gmail.com>
2016-08-09 18:23:29 -0400

    Migrate to using Ocaml-ci scripts
    Update and restrict environment to versions 4.03, 1.2.2

--- tools/travis_ci_test.sh
+++ tools/travis_ci_test.sh
@@ -1,41 +1,17 @@
-#!/usr/bin/env bash
-
-export opam_pin_add=""
 travis_install_on_linux () {
-    # Install OCaml and OPAM PPAs
-    case "$OCAML_VERSION,$OPAM_VERSION" in
-        4.02.0,1.1.0) ppa=avsm/ocaml42+opam11 ;;
-        4.02.0,1.2.0) ppa=avsm/ocaml42+opam12; export opam_pin_add="add" ;;
-      *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
-    esac
-
-    echo "yes" | sudo add-apt-repository ppa:$ppa
-    sudo apt-get update -qq
-
-    sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam time git
-    sudo apt-get install liblapack-dev
-    sudo apt-get install gfortran       # for lbgfs
-    sudo apt-get install libffi-dev     # for Ctypes
+  sudo apt-get install liblapack-dev
+  sudo apt-get install gfortran       # for lbgfs
+  sudo apt-get install libffi-dev     # for Ctypes
 }
 
 travis_install_on_osx () {
-    #curl -OL "http://xquartz.macosforge.org/downloads/SL/XQuartz-2.7.6.dmg"
-    #sudo hdiutil attach XQuartz-2.7.6.dmg
-    #sudo installer -verbose -pkg /Volumes/XQuartz-2.7.6/XQuartz.pkg -target /
-
-    echo "updating brew"
-    brew update > /dev/null
     echo "brew install lapack"
     brew install homebrew/dupes/lapack > /dev/null
     echo "brew install gcc"
     brew install gcc > /dev/null  # for gfortran
     echo "brew install libffi"
     brew install libffi > /dev/null # for ocephes
-    echo "brew install opam"
-    brew install opam > /dev/null
     echo "brew install finished!"
-    export opam_init_options="--comp=$OCAML_VERSION"
-    export opam_pin_add="add"
 }
 
 case $TRAVIS_OS_NAME in
@@ -44,27 +20,10 @@ case $TRAVIS_OS_NAME in
   *) echo "Unknown $TRAVIS_OS_NAME"; exit 1
 esac
 
-# configure and view settings
-export OPAMYES=1
-echo "ocaml -version"
-ocaml -version
-echo "ocamlbuild version"
-ocamlbuild -version
-echo "opam --version"
-opam --version
-echo "git --version"
-git --version
-
-# install OCaml packages
-opam init $opam_init_options
 eval `opam config env`
-
-# Bypass opam bug #1747
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
-
-# not yet in opam
-opam pin add -y dsfo git://github.com/rleonid/dsfo
+export OPAMYES="true"
+opam install ocamlbuild
+opam pin add dsfo git://github.com/rleonid/dsfo
 
 echo Installing Libraries
 make setup
