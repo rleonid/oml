7199282f33d75cb606e6955e06eb083e4de86ee7
Leonid Rozenberg <leonidr@gmail.com>
2016-10-15 01:34:19 -0400

    Update old script

--- scripts/examples/classify_example.ml
+++ scripts/examples/classify_example.ml
@@ -1,4 +1,4 @@
-open Classify
+open Classification
 let feature_map = function | `shortbread -> 0 | `lager -> 1 | `whiskey -> 2 | `porridge -> 3 | `football-> 4 ;;
 let data =
   [
@@ -16,14 +16,14 @@ let data =
     (`Scottish, [`shortbread; `whiskey; `porridge]);
     (`Scottish, [`shortbread; `whiskey])
   ] ;;
-let to_feature l = l |> List.map feature_map |> Array.of_list ;;
-module NB = BinomialNaiveBayes(
+let to_feature l = l |> List.map ~f:feature_map |> Array.of_list ;;
+module NB = Naive_bayes.Binomial(
   struct
     type feature = [ `shortbread | `lager | `whiskey | `porridge | `football ] list
     type clas = [ `English | `Scottish]
     let encoding = to_feature
     let size = 5
   end);;
-let naiveb = NB.estimate ~spec:{NB.default with bernoulli = true} data ;;
+let naiveb = NB.estimate ~opt:( NB.opt ~bernoulli:true ()) data ;;
 let sample = [ `shortbread ; `whiskey; `porridge  ]  ;;
 let result = NB.eval naiveb sample;;
